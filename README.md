90fad416944b1c4c87db0e657598aa2ba3094027
tim.varvikko <tim.varvikko59@gmail.com>
2020-12-08 16:15:55 +0100

    version 1.2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Memory-Game
+Gruppmedlemmar: Tim Varvikko
+
+Ansvarsområde: Tim Varvikko: Flickr API, showImg() funktion, HTML, CSS, Prototype.
+
+Användarmanual: Klicka på Start Game knappen och skriv in ditt namn, en text presenteras. Skriv sedan in vad för slags motiv du vill ha till dina bilder och tryck på Enter. Du ska hitta alla par, när ett par har hittats kommer bilderna inte att vändas om igen och du får en poäng. Tiden kommer börja 1 sekund efter att första kortet vänds.
--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-let imgFront = document.querySelectorAll('.front-card');
-const cards = document.querySelectorAll('.card');
+let imgFront = document.querySelectorAll('.front-card'); //Variabel för att komma åt classen fron-card
+const cards = document.querySelectorAll('.card');  //Variabel för att komma åt classen card
 let counter = 0;
 let firstClick = true;
 let cardPair = [];
 
-let startGame = document.querySelector(".control-button span");
-let helloName = document.querySelector(".name span");
+let startGame = document.querySelector(".control-button span"); //Variabel för att komma åt span elementet i control-button classen 
+let helloName = document.querySelector(".name span");  //Variabel för att komma åt span elementet i name classen 
 
 
 
 
-function User(name){
-	this.name = name;
+function User(name){  //contructor funktion som tar en name parameter
+	this.name = name; //pekar på att this.name är lika med parametern name
 }
 
-startGame.onclick = function () {
-	let yourName = prompt("Whats Your Name?");
-	let UserOne = new User(yourName);
-        if (yourName == null || yourName == "") {
-            helloName.innerHTML = 'Unknown';
+startGame.onclick = function () {  //on click funktion
+	let yourName = prompt("Whats Your Name?"); //Variabel som frågar användaren efter namn med prompt function
+	let UserOne = new User(yourName); //skapar en ny User som kan använda all information från user men lägger till en eget namn med parametern 'yourName'
+        if (yourName == null || yourName == "") { //kollar om namnet som användaren skrev in returnerar null eller tomt fält
+            helloName.innerHTML = 'Unknown'; //om ovanstående är sant så heter spelaren Unknown
         } else {
-            helloName.innerHTML = `Hello ${UserOne.name} and welcome to The Memory Game, please choose a theme of your cards and press Enter`;
+            helloName.innerHTML = `Hello ${UserOne.name} and welcome to The Memory Game, please choose a theme of your cards and press Enter`; //annars skrivs detta till användaren med parametern name från contruktorn User
         }
-		startGame.remove();
+		startGame.remove(); //tar bort startGame knappen
 };
 
 
 
-let input = document.querySelector('.input')
-    input.addEventListener('keypress', function(event){
-        if(event.key === 'Enter'){
+let input = document.querySelector('.input')  //Variabel för att komma åt input classen 
+    input.addEventListener('keypress', function(event){ //Event listener med keypress
+        if(event.key === 'Enter'){ //Om funktionens event key är lika med Enter körs funktionen
 			
-            const key = '020d921e01bcc08823ec27215c7b82d2';
-            let url = `https://api.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${input.value}&per_page=12&page=1&sort=relevance&safe_search=2&format=json&nojsoncallback=1`;
-            fetch(url).then(
+            const key = '020d921e01bcc08823ec27215c7b82d2'; //Variabel till api nyckel
+            let url = `https://api.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${input.value}&per_page=12&page=1&sort=relevance&safe_search=2&format=json&nojsoncallback=1`; //Flickr Api som hämtar bilder med metod flickr.photos.search, text är lika med vad användaren lämnar för value i input, hämtar 12 bilder per sida från första sidan och de mest relevanta till sökningen, satte även safe search till 2 och sen i vilket format som då är json
+            fetch(url).then(  //fetch som hämtar från url variabeln ovanför och sen utför en funktion med parametern 'r'
                 function(r){
-                    return r.json();
+                    return r.json(); //returnerar r till json format
                 }
-            ).then(
+            ).then( //sen utför funktion med parametern 'data'
                 function(data){
-					showImg(data);
+					showImg(data); //Använder datan från json, i funktionen specificeras vilken information vi vill få ut
                     play();
                 }
-            ).catch(
-                function(error){
-                    console.log(error)
+            ).catch( //fångar upp om något blir fel
+                function(error){ //funktion med error parameter
+                    console.log(error) //skriver ut errorn som fångas upp av catch och loggar det i konsolen
                 }
             )
                 
@@ -57,14 +57,14 @@ let input = document.querySelector('.input')
 
 function showImg(data) {
 
-	let urls = [];
-	for (let i = 0; i < data.photos.photo.length; i++) {
-		let serverId = data.photos.photo[i].server
-		let id = data.photos.photo[i].id
-		let secret = data.photos.photo[i].secret
+	let urls = []; //tom array
+	for (let i = 0; i < data.photos.photo.length; i++) {  //for loop som börjar på 0, loopar så länge i är mindre än längden på datan från json flickr i photos photo, ökar med 1
+		let serverId = data.photos.photo[i].server  //Variabel specificerat på index platsen i photo och server id:et
+		let id = data.photos.photo[i].id  //Variabel specificerat på index platsen i photo och id:et
+		let secret = data.photos.photo[i].secret  //Variabel specificerat på index platsen i photo och secret
 
-		let url = `https://live.staticflickr.com/${serverId}/${id}_${secret}_q.jpg`
-		urls[i] = url;
+		let url = `https://live.staticflickr.com/${serverId}/${id}_${secret}_q.jpg` //för att kunna visa bilden behövs dessa tre parametrar fyllas i från ovanstående variabler
+		urls[i] = url;  //lägger in url i urls arrayn
 	}
 
 	let k = 0;
